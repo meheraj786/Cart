94b6edd5094dcc1b2c63754220e5df36f7df6e4a
Mehraj Hosen <meherajhosen786@gmail.com>
2025-04-18 21:53:20 +0600

    price get

--- script.js
+++ script.js
@@ -4,42 +4,42 @@ const products=
   img: 'https://5.imimg.com/data5/KC/PC/MY-38629861/dummy-chronograph-watch.jpg',
   title: 'Lorem Watch',
   description: 'Stylish, elegant, super watch',
-  price: '$100.00'
+  price: 100.00
 },
 {
   id:2,
   img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmyib2yh6G255_f2sPPcpl9DisYjb1osty4codJdDh2vHp1PwOGRXiwXMKBQooUR6Mb0Q&usqp=CAU',
   title: 'Lorem Camera',
   description: 'Stylish, elegant, super camera',
-  price: '$500.00'
+  price: 500.00
 },
 {
   id:3,
   img: 'https://images.unsplash.com/photo-1572635196237-14b3f281503f?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D',
   title: 'Lorem Sunglass',
   description: 'Stylish, elegant, super sunglass',
-  price: '$50.00'
+  price: 50.00
 },
 {
   id:4,
   img: 'https://images.unsplash.com/photo-1583394838336-acd977736f90?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D',
   title: 'Lorem Headphone',
   description: 'Stylish, elegant, super Headphone',
-  price: '$200.00'
+  price: 200.00
 },
 {
   id:5,
   img: 'https://images.unsplash.com/photo-1546868871-7041f2a55e12?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D',
   title: 'Lorem Smart Watch',
   description: 'Stylish, elegant, super smart watch',
-  price: '$150.00'
+  price: 150.00
 },
 {
   id:6,
   img: 'https://images.unsplash.com/photo-1560343090-f0409e92791a?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D',
   title: 'Lorem Shoe',
   description: 'Stylish, elegant, super shoe',
-  price: '$300.00'
+  price: 300.00,
 },
 ]
 const main= document.querySelector('#main')
@@ -50,6 +50,7 @@ const cartList= document.querySelector("#sidebar")
 
 
 let cart=[]
+let price=0
 
 products.map((product)=>{
   const productBox= document.createElement("div")
@@ -62,7 +63,8 @@ productBox.innerHTML=`
         <h2 class="product-name">${product.title}</h2>
         <p class="product-description">${product.description}</p>
         <div class="product-selection">
-          <p class="product-price">${product.price}</p>
+          <p class="product-price">$
+          ${product.price}</p>
           <div class="number">
             <button class="decrease">-</button>
             <input type="number" min="1" name="" value="1" id=""><button class="increase">+</button>
@@ -74,13 +76,17 @@ productBox.innerHTML=`
 main.appendChild(productBox)
 })
 function addToCart(productId) {
-  console.log("click");
   products.find((product)=>{
     if (productId===product.id) {
       cart.push(product)
       displayCart()
     }
   })
+  price=0
+  cart.map((product)=>{
+    price+=product.price
+  })
+  console.log(price);
 
   
 }
@@ -120,6 +126,8 @@ function displayCart() {
     `;
     cartList.appendChild(cardItem);
   });
+
+
 }
 
 cartBtn.addEventListener("click", ()=>{
