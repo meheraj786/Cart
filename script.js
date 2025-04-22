fadf1b0b154b85ae5bd089b51a64d304c27025b4
Mehraj Hosen <meherajhosen786@gmail.com>
2025-04-22 15:16:38 +0600

    more products add

--- script.js
+++ script.js
@@ -4,35 +4,40 @@ const products=
   img: 'https://5.imimg.com/data5/KC/PC/MY-38629861/dummy-chronograph-watch.jpg',
   title: 'Lorem Watch',
   description: 'Stylish, elegant, super watch',
-  price: 100.00
+  price: 100.00,
+  category: 'Accesories'
 },
 {
   id:2,
   img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmyib2yh6G255_f2sPPcpl9DisYjb1osty4codJdDh2vHp1PwOGRXiwXMKBQooUR6Mb0Q&usqp=CAU',
   title: 'Lorem Camera',
   description: 'Stylish, elegant, super camera',
-  price: 500.00
+  price: 500.00,
+  category: 'Accesories'
 },
 {
   id:3,
   img: 'https://images.unsplash.com/photo-1572635196237-14b3f281503f?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D',
   title: 'Lorem Sunglass',
   description: 'Stylish, elegant, super sunglass',
-  price: 50.00
+  price: 50.00,
+  category: 'Accesories'
 },
 {
   id:4,
   img: 'https://images.unsplash.com/photo-1583394838336-acd977736f90?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D',
   title: 'Lorem Headphone',
   description: 'Stylish, elegant, super Headphone',
-  price: 200.00
+  price: 200.00,
+  category: 'Accesories'
 },
 {
   id:5,
   img: 'https://images.unsplash.com/photo-1546868871-7041f2a55e12?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D',
   title: 'Lorem Smart Watch',
   description: 'Stylish, elegant, super smart watch',
-  price: 150.00
+  price: 150.00,
+  category: 'Accesories'
 },
 {
   id:6,
@@ -40,6 +45,55 @@ const products=
   title: 'Lorem Shoe',
   description: 'Stylish, elegant, super shoe',
   price: 300.00,
+  category: 'Shoes'
+},
+{
+  id:7,
+  img: 'https://images.pexels.com/photos/769749/pexels-photo-769749.jpeg?auto=compress&cs=tinysrgb&w=400',
+  title: 'Lorem Check Shirt',
+  description: 'Stylish, elegant, super Shirt',
+  price: 600.00,
+  category: 'Topwear'
+},
+{
+  id:8,
+  img: 'https://images.pexels.com/photos/1656684/pexels-photo-1656684.jpeg?auto=compress&cs=tinysrgb&w=400',
+  title: 'Lorem Black T-Shirt',
+  description: 'Stylish, elegant, super Shirt',
+  price: 500.00,
+  category: 'Topwear'
+},
+{
+  id:9,
+  img: 'https://images.pexels.com/photos/2887775/pexels-photo-2887775.jpeg?auto=compress&cs=tinysrgb&w=400',
+  title: 'Lorem Coke T-Shirt',
+  description: 'Stylish, elegant, super Shirt',
+  price: 450.00,
+  category: 'Topwear'
+},
+{
+  id:10,
+  img: 'https://images.pexels.com/photos/4066290/pexels-photo-4066290.jpeg?auto=compress&cs=tinysrgb&w=400',
+  title: 'Lorem Jeans Pant',
+  description: 'Stylish, elegant, super Pant',
+  price:750.00,
+  category: 'Bottomwear'
+},
+{
+  id:11,
+  img: 'https://images.pexels.com/photos/2897533/pexels-photo-2897533.jpeg?auto=compress&cs=tinysrgb&w=400',
+  title: 'Lorem Check Pant',
+  description: 'Stylish, elegant, super Pant',
+  price:700.00,
+  category: 'Bottomwear'
+},
+{
+  id:12,
+  img: 'https://images.pexels.com/photos/7764611/pexels-photo-7764611.jpeg?auto=compress&cs=tinysrgb&w=400',
+  title: 'Lorem Sky-Blue Pant',
+  description: 'Stylish, elegant, super Pant',
+  price:800.00,
+  category: 'Bottomwear'
 },
 ]
 const main = document.querySelector('#main');
